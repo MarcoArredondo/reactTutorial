4c5fb9181d9ec14dcdf355c1eb4ae6b06c368925
Marco Arredondo <marcoarredondoz28@gmail.com>
2020-11-06 12:07:38 -0600

    * Creation of firsts components * Example on adding style

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Imporing Javascript files dont need to have the extension file
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Importing other type of files need the full path
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Function to declare components needs to start with an Uppercase
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+//For inline style in JSX you need a Javascrip object and using the Javascript properties for the style
+function Book() {
+  return (
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/41+rguupalL._SX322_BO1,204,203,200_.jpg"
+        alt="book"
+      />
+      <h1>El Hombre Duplicado</h1>
+
+      <h4
+        style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
+      >
+        Jose Saramago
+      </h4>
+    </article>
+  );
+}
+
+ReactDom.render(<BookList />, document.getElementById("root"));
