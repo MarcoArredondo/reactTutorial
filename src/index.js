6383502fc7be663eb57787dd08fb649362666548
Marco Arredondo <marcoarredondoz28@gmail.com>
2020-11-07 14:42:52 -0600

    * Passing propos into components

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import ReactDom from "react-dom";
 
 //Importing other type of files need the full path
 import "./index.css";
+const firstBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/41+rguupalL._SX322_BO1,204,203,200_.jpg",
+  title: "El Hombre Duplicado",
+  author: "Jose Saramago",
+};
 
 //Function to declare components needs to start with an Uppercase
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
       <Book />
       <Book />
     </section>
@@ -17,19 +27,17 @@ function BookList() {
 }
 
 //For inline style in JSX you need a Javascrip object and using the Javascript properties for the style
-function Book() {
+function Book(props) {
+  console.log(props);
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/41+rguupalL._SX322_BO1,204,203,200_.jpg"
-        alt="book"
-      />
-      <h1>El Hombre Duplicado</h1>
+      <img src={props.img} alt="book" />
+      <h1>{props.title}</h1>
 
       <h4
         style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
       >
-        Jose Saramago
+        {props.author}
       </h4>
     </article>
   );
